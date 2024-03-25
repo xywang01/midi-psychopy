561430f834abecb04d036ef199ab736ab834abf5
Michael <xywang02@gmail.com>
2024-03-25 15:30:09 -0400

    created package setup and init

---
+++ midi_psychopy/__init__.py
@@ -0,0 +1,3 @@
+from .functions import *
+from .midi_interface import MidiInterface
+from .midi_controller import MidiController
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='midi-psychopy',
+    version='0.0.1',
+    description='A Python interface for asynchronous MIDI data collection using PsychoPy',
+    url='https://github.com/xywang01/midi-psychopy',
+    download_url='',
+    author='X. Michael Wang, Centre for Motor Control, University of Toronto, Faculty of Kinesiology and Physical Education',
+    author_email='michaelwxy.wang@utoronto.ca',
+    license='MIT',
+    packages=['midi_psychopy', ],
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'mido',
+        'numpy',
+        'pandas',
+        'matplotlib',
+    ]
+)
